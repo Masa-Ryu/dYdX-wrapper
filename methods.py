f00eebdc0ece3b4e5fc4fda366352c5da73e83f6
Masa-Ryu <privatead.ryu@gmail.com>
2022-02-05 13:22:07 +0900

    New adapt

--- methods.py
+++ methods.py
@@ -21,12 +21,78 @@ class DydxMethod:
                 web3=Web3(Web3.HTTPProvider(web_provider_url)),
                 default_ethereum_address=self._ETHEREUM_ADDRESS,
                 stark_private_key=self._STARK_KEY,
-                api_key_credentials=self._API_KEY
+                api_key_credentials=self._API_KEY,
                 )
 
+    @staticmethod
+    def a(arg):
+        result = arg.data
+        return result
+
+    def authentication(self):
+        user = self.a(self.get_user())
+        if self._ETHEREUM_ADDRESS.lower() == str(
+                user['user']['ethereumAddress']
+                ):
+            return 'True'
+        else:
+            raise ConnectionError('Authentication Failed')
+
+    def get_free_balance(self):
+        account = self.a(self.client.private.get_account())
+        return float(account['account']['freeCollateral'])
+
+    def get_position(self, market):
+        account = self.a(
+                self.client.private.get_positions(
+                        market=market,
+                        status=POSITION_STATUS_OPEN,
+                        )
+                )
+        if not account['positions']:
+            return 0
+        else:
+            for position in account['positions']:
+                if position['market'] == market:
+                    return float(position['size'])
+
+    def get_price(self, market):
+        orderbook = self.a(self.client.public.get_markets(market=market))
+        return float(orderbook['markets'][market]['indexPrice'])
+
+    def place_order(
+            self, market, side, type_, size, expiration, price=None,
+            post_only=False, limit_fee=0.005, time_in_force=None,
+            trigger_price=None
+            ):
+        account_response = self.client.private.get_account()
+        params = {
+                'position_id': account_response.data['account']['positionId'],
+                'market': market,  # BTC-PERP
+                'side': side,  # buy or sell
+                'order_type': type_,  # limit or market
+                'size': str(size),
+                'post_only': post_only,
+                'limit_fee': limit_fee,
+                'expiration_epoch_seconds': expiration,
+                }
+        if type_ == 'market':
+            price = None
+        if price is not None:
+            params['price'] = str(price)
+        if trigger_price is not None:
+            params['triggerPrice'] = str(trigger_price)
+        if time_in_force is not None:
+            params['time_in_force'] = time_in_force
+        order = self.a(self.client.private.create_order(**params))
+        return order['order']
+
+    def fills(self, market):
+        all_fills = self.a(self.client.private.get_fills(market=market))
+        return all_fills['fills']
+
     def get_user(self):
-        user = self.client.private.get_user()
-        print(user.data)
+        return self.client.private.get_user()
 
     def register_api_key(self):
         api_key_response = self.client.eth_private.create_api_key()
